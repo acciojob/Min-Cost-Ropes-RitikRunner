3a7fb7b54fe16fa089a8037a569b650a2bf8bf08
Ritik Sharma <ritiksharma10508@gmail.com>
2026-01-13 22:51:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+
+let arr = [4, 3, 2, 6]
 function mincost(arr)
 { 
+	let sum = 0;
+	for(let i=0;i<=arr.length;i++){
+		sum = sum+arr[i]
+		return sum
+	}
+mincost(arr)	
 //write your code here
 // return the min cost
   
